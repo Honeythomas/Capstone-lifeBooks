f7e8c25aeac9e1beb7bbe0f0da0987d36e0d2014
Honeythomas <117334674+Honeythomas@users.noreply.github.com>
2023-02-16 14:00:00 +0530

    LifeCoach Prototype

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,35 @@
+import React, { useContext } from "react";
+import { themeContext } from "../../Context";
+import "./Experience.css";
+const Experience = () => {
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
+  return (
+    <div className="experience" id="experience">
+      <div className="achievement">
+        {/* darkMode */}
+        <div
+          className="circle"
+          style={{ color: darkMode ? "var(--orange)" : "" }}
+        >
+          10
+        </div>
+        <span style={{ color: darkMode ? "white" : "" }}>Website </span>
+        <span>Rating</span>
+      </div>
+      <div className="achievement">
+        <div
+          className="circle"
+          style={{ color: darkMode ? "var(--orange)" : "" }}
+        >
+          1000+
+        </div>
+        <span style={{ color: darkMode ? "white" : "" }}>Involving </span>
+        <span>People</span>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,93 @@
+import React, { useContext } from "react";
+import "./Intro.css";
+import Vector1 from "../../img/Vector1.png";
+import Vector2 from "../../img/Vector2.png";
+import boy from "../../img/boy.png";
+import glassesimoji from "../../img/glassesimoji.png";
+import thumbup from "../../img/thumbup.png";
+import crown from "../../img/crown.png";
+import FloatinDiv from "../FloatingDiv/FloatingDiv";
+import Github from "../../img/github.png";
+import LinkedIn from "../../img/linkedin.png";
+import Instagram from "../../img/instagram.png";
+import { themeContext } from "../../Context";
+import { motion } from "framer-motion";
+import { Link } from "react-scroll";
+const Intro = () => {
+  // Transition
+  const transition = { duration: 2, type: "spring" };
+
+  // context
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
+  return (
+    <div className="Intro" id="Intro">
+      {/* left name side */}
+      <div className="i-left">
+        <div className="i-name">
+          {/* yahan change hy darkmode ka */}
+          <span style={{ color: darkMode ? "white" : "" }}>
+            Benefits of learning through "Life Coach"
+          </span>
+          <span>Honey Thomas/Mohsin Ali/Rahil Khan</span>
+          <span>
+            Life coaching involves working with clients to help improve their
+            quality of life through various emotions and actions. A Life Coach
+            will be there to support a client throughout the coaching process
+            until they feel they have reached the goals they have set in place.
+            Life coaches will create a mentoring relationship in which clients
+            feel confident and comfortable. There are three stages to mentoring
+            relationships and the process it goes through to reach the end goal.
+            Cultivation – This is where the mentee will learn from the
+            experience of the mentor. Whilst this is happening the mentor is
+            gathering knowledge and insights on the mentee to have a clear
+            understanding of emerging issues. Separation – This stage would be
+            the end of a relationship, however, challenges tend to arise at this
+            point if one of the members is not ready to end the relationship.
+            This may be because the mentee disagrees with the process the mentor
+            thinks they have made. Redefinition – This is the point where the
+            relationship reaches the same level after successfully working
+            through the separation stage. This would mean both members feel
+            confident and comfortable in the relationship ending.
+          </span>
+        </div>
+
+        {/* social icons */}
+        <div className="i-icons">
+          <img src={Github} alt="" />
+          <img src={LinkedIn} alt="" />
+          <img src={Instagram} alt="" />
+        </div>
+      </div>
+      {/* right image side */}
+      <div className="i-right">
+        <img src={Vector1} alt="" />
+        <img src={Vector2} alt="" />
+        <img src={boy} alt="" />
+        {/* animation */}
+        <motion.img
+          initial={{ left: "-36%" }}
+          whileInView={{ left: "-24%" }}
+          transition={transition}
+          src={glassesimoji}
+          alt=""
+        />
+
+        <div className="blur" style={{ background: "rgb(238 210 255)" }}></div>
+        <div
+          className="blur"
+          style={{
+            background: "#C1F5FF",
+            top: "17rem",
+            width: "21rem",
+            height: "11rem",
+            left: "-9rem",
+          }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
