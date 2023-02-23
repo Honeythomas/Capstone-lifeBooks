fa3bb30bae9b30c06ca04b6aabc54ac009d9a8c1
mohsinali0001 <alimohsin222111@gmail.com>
2023-02-24 02:48:40 +0530

    fixed some componets

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,5 @@
+
+
 import React from "react";
 import "./Footer.css";
 import Wave from "../../img/wave.png";
@@ -10,7 +12,7 @@ const Footer = () => {
     <div className="footer">
       <img src={Wave} alt="" style={{ width: "100%" }} />
       <div className="f-content">
-        <span>lifeCoach@gmail.com</span>
+        <span>Zainkeepscode@gmail.com</span>
         <div className="f-icons">
           <Insta color="white" size={"3rem"} />
           <Facebook color="white" size={"3rem"} />
